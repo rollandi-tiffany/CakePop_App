2137caf4adc2ef811855dd7d258d73210bfce2a9
Tiffany Torres <torresrtiffany@gmail.com>
2023-08-18 02:15:19 -0700

    add authController

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const app = express();
 const PORT = 3000;
 //const expressLayouts = require("express-ejs-layouts");
 const authRoutes = require("./controllers/authController");
+const session = require("express-session");
+const cakepopRoutes = require("./controllers/cakepopController");
 
 app.set("view engine", "ejs");
 
 //middlewares
 app.use(express.static("public"));
 //app.use(expressLayouts);
+app.use(session({ secret: "timeout", cookie: { maxAge: 3600000 }}));
 
 
 
@@ -20,7 +23,15 @@ app.use(authRoutes);
 app.get("/", (req, res) => {
     res.render("home.ejs");
 });
+ app.use((req, res, next) =>{
+    if(!req.session.userId){
+        res.redirect("/login");
+        return;
+    }
 
+    next();
+ });
 
+ app.use("/cakepop", cakepopRoutes);
 
 app.listen(PORT, () => console.log("Hello, from port:", PORT));
